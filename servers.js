ba28dff28be76ca890532be2fcf476c8d6b6f0b8
VanderLord008 <90377070+VanderLord008@users.noreply.github.com>
2023-12-29 22:39:16 +0530

    Update servers.js

--- servers.js
+++ servers.js
@@ -3,16 +3,19 @@ const dotenv = require("dotenv");
 const express = require("express");
 dotenv.config();
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
+aapp.use(function(req, res, next) {
+      // res.header("Access-Control-Allow-Origin", "*");
+      const allowedOrigins = ['http://localhost:3000', 'https://agarbacktrial.onrender.com', 'https://agarbacktrial.onrender.com','https://famous-platypus-3a5ce8.netlify.app'];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+           res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      res.header("Access-Control-Allow-credentials", true);
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+      next();
+    });
 const PORT =5000;
 app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(PORT);
